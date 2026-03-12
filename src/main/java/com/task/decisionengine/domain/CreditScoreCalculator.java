01932c54a5f79d1965824b1c7a74a4fffa4a8aab
Wiktor Wilński <wilinskiwiktor27@gmail.com>
2026-03-12 12:38:48 +0100

    refactor code in calculator

--- src/main/java/com/task/decisionengine/domain/CreditScoreCalculator.java
+++ src/main/java/com/task/decisionengine/domain/CreditScoreCalculator.java
@@ -11,36 +11,36 @@ class CreditScoreCalculator {
         int creditModifier = userProfileRegistry.findUserCreditModifierByPersonalCode(loanInfo.personalCode());
 
         if (creditModifier <= 0) {
-            return LoanDecisionReportDto.builder()
-                    .outcome(DecisionEngineOutcome.NEGATIVE)
-                    .amount(0)
-                    .period(loanInfo.period())
-                    .build();
+            return buildNegativeOutcome(loanInfo.period());
         }
 
         int maxAmount = creditModifier * loanInfo.period();
 
         if (maxAmount >= LoanValidator.MIN_LOAN_AMOUNT) {
-            return LoanDecisionReportDto.builder()
-                    .outcome(DecisionEngineOutcome.POSITIVE)
-                    .amount(Math.min(maxAmount, LoanValidator.MAX_LOAN_AMOUNT))
-                    .period(loanInfo.period())
-                    .build();
+            return buildPositiveOutcome(maxAmount, loanInfo.period());
         }
-
 
         int requiredPeriod = LoanValidator.MIN_LOAN_AMOUNT / creditModifier;
 
         if(requiredPeriod <= LoanValidator.MAX_LOAN_PERIOD){
             int finalPeriod = Math.max(requiredPeriod, LoanValidator.MIN_LOAN_PERIOD);
             int finalAmount = Math.min(creditModifier * finalPeriod, LoanValidator.MAX_LOAN_AMOUNT);
-            return LoanDecisionReportDto.builder()
-                    .outcome(DecisionEngineOutcome.POSITIVE)
-                    .amount(finalAmount)
-                    .period(finalPeriod)
-                    .build();
+            return buildPositiveOutcome(finalAmount, finalPeriod);
         }
 
-        return new LoanDecisionReportDto(DecisionEngineOutcome.NEGATIVE, 0, loanInfo.period());
+        return buildNegativeOutcome(loanInfo.period());
     }
+
+    private LoanDecisionReportDto buildNegativeOutcome(int period){
+        return new LoanDecisionReportDto(DecisionEngineOutcome.NEGATIVE, 0, period);
+    }
+
+    private LoanDecisionReportDto buildPositiveOutcome(int amount, int period){
+        return LoanDecisionReportDto.builder()
+                .outcome(DecisionEngineOutcome.POSITIVE)
+                .amount(Math.min(amount, LoanValidator.MAX_LOAN_AMOUNT))
+                .period(period)
+                .build();
+    }
+
 }
